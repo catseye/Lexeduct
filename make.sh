47d4b3a62585804c513e55e094959f1e8b882e49
Chris Pressey <cpressey@catseye.tc>
2022-09-09 14:39:33 +0100

    The Python build script is written in Python 3.

--- make.sh
+++ make.sh
@@ -5,5 +5,5 @@ cd src
 if [ x`which $NODE` != x ]; then
     $NODE gen-lexeduct-transformers.js >../demo/lexeduct-transformers.js
 else
-    python gen-lexeduct-transformers.py >../demo/lexeduct-transformers.js
+    python3 gen-lexeduct-transformers.py >../demo/lexeduct-transformers.js
 fi
--- src/gen-lexeduct-transformers.py
+++ src/gen-lexeduct-transformers.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 
+
 def write(s):
     sys.stdout.write(s + "\n")
+
 
 def main(args):
     write("module = {};")
@@ -17,7 +19,8 @@ def main(args):
             transformer_name = filename.split('.js')[0]
             with open(os.path.join(dirname, filename)) as f:
                 write(f.read())
-            write("transformer['%s'] = module.exports;" % transformer_name)
+            write("transformer['{}'] = module.exports;".format(transformer_name))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
